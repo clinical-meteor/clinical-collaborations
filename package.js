5f696c527480f73f9090ed07fa5cb77d7aa681af
Abigail Watson <awatson1978@gmail.com>
2016-12-03 21:12:08 -0600

    circle.yml

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 
 Package.describe({
   name: 'clinical:collaborations',
-  version: '2.3.10',
+  version: '2.4.0',
   summary: 'Collaboration based security architecture (similar to Roles and Friends)',
   git: 'https://github.com/clinical-meteor/clinical-collaborations',
   documentation: 'README.md'
@@ -14,27 +14,21 @@ Package.onUse(function (api) {
 
   api.use([
     'mongo@1.1.0',
-    'mongo-livedata@1.0.8',
-    'minimongo@1.0.8',
-    'accounts-base@1.2.0',
-    'accounts-password@1.1.1',
+    'accounts-base@1.2.14',
+    'accounts-password@1.3.3',
     'aldeed:simple-schema@1.3.3',
     'aldeed:collection2@2.5.0',
-    'grove:less@0.1.1',
-    'http@1.1.0',
-    'underscore@1.0.3',
+    'http@1.1.8',
+    'underscore@1.0.10',
     'yasaricli:slugify@0.0.7',
-    'clinical:base-model@1.3.4',
+    'clinical:base-model@1.3.5',
     'matb33:collection-hooks@0.8.1',
-    'clinical:user-model@1.4.2'
+    'clinical:user-model@1.6.0'
   ], ['client', 'server']);
 
-  api.imply('mongo');
-  api.imply('mongo-livedata');
-  api.imply('minimongo');
-  api.imply('accounts-base');
-  api.imply('accounts-password');
-  api.imply('clinical:user-model');
+  api.imply('accounts-base@1.2.14');
+  api.imply('accounts-password@1.3.3');
+  api.imply('clinical:user-model@1.6.0');
 
   api.addFiles([
     'client/subscriptions.js',
@@ -59,27 +53,4 @@ Package.onUse(function (api) {
     'Collaborations',
     'Collaboration'
   ]);
-});
-
-
-
-
-Package.onTest(function (api) {
-  api.versionsFrom('1.1.0.2');
-
-  api.use('meteor-platform@1.2.2');
-
-  api.use('mongo@1.1.0');
-  api.use('mongo-livedata@1.0.8');
-  api.use('minimongo@1.0.8');
-
-  api.use('accounts-base@1.2.0');
-  api.use('accounts-password@1.1.1');
-  api.use('autopublish@1.0.3');
-  api.use('insecure@1.0.3');
-  api.use('tinytest@1.0.5');
-  api.use('clinical:verification');
-  api.use('clinical:collaborations');
-
-  api.addFiles('tests/tinytests/collaborations.js');
 });
